a570457d56d4b3527c30013b6313bb956de8e07c
xchancusUTC <myriam.chancusi@outlook.com>
2016-11-14 09:54:00 -0500

    1

---
+++ play-ebean-example-master/app/models/computadoras.java
@@ -0,0 +1,38 @@
+package models;
+
+
+
+
+import java.util.*;
+
+import javax.persistence.*;
+import javax.validation.Constraint;
+
+import play.db.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+import com.avaje.ebean.*;
+
+@Entity
+
+public class computadoras extends Model {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+    
+	public Long id;
+    
+    @Constraints.Required(message=" ingresar nombre de la computadora.")
+    public String nombre;
+    
+    @Constraints.Required(message=" ingresar su memoria.")
+    public String descripcion;
+    
+    @Constraints.Required(message=" ingresar su disco.")
+    public String descripcio;
+    
+    @Constraints.Required(message="marca")
+
+}
